98d6fe1b13a69fd3b1ecb8e766380120d6bb5030
noor19-meet <noor19@meet.mit.edu>
2017-08-09 16:55:27 +0300

    Stopping here

---
+++ edges.py
@@ -0,0 +1,13 @@
+if new_x_pos_1 >= RIGHT_EDGE:
+    truck1.goto(LEFT_EDGE, new_y_pos_1)
+if new_x_pos_1 <= LEFT_EDGE:
+    truck1.goto(RIGHT_EDGE, new_y_pos_1)
+
+
+
+if new_x_pos_2 >= RIGHT_EDGE:
+    truck1.goto(LEFT_EDGE, new_y_pos_2)
+if new_x_pos_2 <= LEFT_EDGE:
+    truck1.goto(RIGHT_EDGE, new_y_pos_2)
+    
+
