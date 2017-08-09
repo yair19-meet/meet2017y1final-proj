aff5519a67f3179170e30bae6de5be2847067220
noor19-meet <noor19@meet.mit.edu>
2017-08-09 14:28:10 +0300

    Stopping here

---
+++ Timer.py
@@ -0,0 +1,13 @@
+import turtle
+import time
+
+
+for m in range (1):
+    for s in range (60):
+       turtle.clear()
+       turtle.write(str(m) + ": " + str(s))
+
+       time.sleep(1)
+       
+
+turtle.mainloop
--- noor.py
+++ noor.py
@@ -9,7 +9,7 @@ turtle.penup()
 
 SQUARE_SIZE = 20
 
-turtle.bgcolor('blue')
+turtle.bgcolor ('gray')
 
 #Initialize lists
 unhealthy_food_pos = []
@@ -17,23 +17,43 @@ healthy_food_pos = []
 unhealthy_food_stamps = []
 healthy_food_stamps = []
 
+TIME_STEP = 1500
+turtle.hideturtle()
+
 
 
 healthy_food = turtle.clone()
-turtle.register_shape("frygif.gif") #add apple picture
-healthy_food.shape("frygif.gif")
+turtle.register_shape("apple.gif") #add apple picture
+healthy_food.shape("apple.gif")
+
+#healthy_food_pos = [(100,100)]
+#healthy_food_stamps = []
+
+for this_healthy_food_pos in healthy_food_pos:
+    healthy_food.goto(this_healthy_food_pos)
+    healthyfood1 = healthy_food.stamp()
+    healthy_food_stamps.append(healthyfood1)
 
 
-#unhealthy_food = turtle.clone()
-#unhealthy_food.register_shape("giphy.gif") #add chips picture 
-#unhealthy_food.shape("giphy.gif")
+unhealthy_food = turtle.clone()
+turtle.register_shape("frygif.gif") #add chips picture 
+unhealthy_food.shape("frygif.gif")
 
-#turtle.shape(".....")
+#unhealthy_food_pos = [(-100,100)]
+#unhealthy_food_stamps = []
+
+for this_unhealthy_food_pos in unhealthy_food_pos:
+    unhealthy_food.goto(this_unhealthy_food_pos)
+    unhealthyfood1 = unhealthy_food.stamp()
+    unhealthy_food_stamps.append(unhealthyfood1)
+
+
 
 def make_healthyfood():
     #the screen positions go from -SIZE/2 to +SIZE/2
     #we need to make food pieces only appear on game squares
     #so we cut up the game board into multiples of SQUARE_SIZE
+    global TIME_STEP
     min_x = -int(SIZE_X/2/SQUARE_SIZE) + 1
     max_x = int(SIZE_X/2/SQUARE_SIZE) - 1
     min_y = -int(SIZE_Y/2/SQUARE_SIZE) + 1
@@ -50,11 +70,18 @@ def make_healthyfood():
     healthy_food_stamp1 = healthy_food.stamp()
     healthy_food_stamps.append(healthy_food_stamp1)
 
+    TIME_STEP = 1800
+
+    turtle.ontimer(make_healthyfood,TIME_STEP)
+
+    
+
 def make_unhealthyfood():
-    min_x1 = -int(SIZE_X/2/SQUARE_SIZE) + 3
-    max_x1 = int(SIZE_X/2/SQUARE_SIZE) - 3
-    min_y1 = -int(SIZE_Y/2/SQUARE_SIZE) + 3
-    max_y1 = int(SIZE_Y/2/SQUARE_SIZE) - 3
+    global TIME_STEP
+    min_x1 = -int(SIZE_X/2/SQUARE_SIZE) + 1
+    max_x1 = int(SIZE_X/2/SQUARE_SIZE) - 1
+    min_y1 = -int(SIZE_Y/2/SQUARE_SIZE) + 1
+    max_y1 = int(SIZE_Y/2/SQUARE_SIZE) - 1
 
     
     unhealthy_food_x = random.randint(min_x1, max_x1) * SQUARE_SIZE
@@ -66,4 +93,18 @@ def make_unhealthyfood():
     unhealthy_food_pos.append (unhealthy_food_pos1)
     unhealthy_food_stamp1 = unhealthy_food.stamp()
     unhealthy_food_stamps.append(unhealthy_food_stamp1)
+
+    TIME_STEP = 4000
+
+    turtle.ontimer(make_unhealthyfood,TIME_STEP)
+
+
+
+make_healthyfood()
+make_unhealthyfood()
+
+    
+    
+    
+    
 
